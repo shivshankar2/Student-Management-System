ce64c29919c5defecdb9003efe476889cc7a74b3
shivshankar2 <88234755+shivshankar2@users.noreply.github.com>
2022-04-14 09:43:46 +0530

    Add files via upload

---
+++ layout22.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="<a href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css%22>" rel="nofollow"><u>https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></u></a>
+    <script src="<a href="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js%22></script>" rel="nofollow"><u>https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script></u></a>
+    <script src="<a href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js%22></script>" rel="nofollow"><u>https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script></u></a>
+
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+        
+
+        
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+     
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Laravel</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/myhome">Home</a></li>
+            <li><a href="list">UserList</a></li>
+            <li><a href="/">Login</a></li>
+            <li><a href="Create">Create Account</a></li>
+            
+          </ul>
+        </div>
+      </nav>  
+</body>
+</html>
