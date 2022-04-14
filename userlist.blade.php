6da98b2e3c3309a30b5c6bbaf3cf96976a233719
shivshankar2 <88234755+shivshankar2@users.noreply.github.com>
2022-04-14 09:45:05 +0530

    Add files via upload

---
+++ userlist.blade.php
@@ -0,0 +1,40 @@
+
+
+@extends('layout')
+@section('content')
+<h1>Student Information</h1>
+
+<img src="/images/abc.jpj" alt="">
+ 
+<div class="scrollmenu">
+  <a href="#home">Student Profile</a> &nbsp;&nbsp;
+  <a href="#news">Fee Structure</a>&nbsp;&nbsp;
+  <a href="#contact">New Term Guideline</a>&nbsp;&nbsp;
+  <a href="#about">Placement Portal</a>&nbsp;&nbsp;
+  ...
+
+</div>
+<br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<textarea name="message" rows="10" cols="30">
+<b>!! NATIONAL ANTHEM SONG!!</b>
+
+jana-gaṇa-mana-adhināẏaka jaẏa hē Bhārata-bhāgya-bidhātā[b]!
+Pañjāba Sindh Gujarāṭa Marāṭhā Drābiṛa[c] Utkala Baṅga[d]
+Bindhya[e] Himācala Yamunā Gaṅgā Ucchala-jaladhi-taraṅga
+taba[f] śubha nāmē jāgē, taba[f] śubha āśiṣa māgē,
+gāhē taba[f] jaẏagāthā.
+jana-gaṇa-maṅgala-dāẏaka jaẏa hē bhārata-bhāgya-bidhātā[b]!
+jaẏa hē, jaẏa hē, jaẏa hē, jaẏa jaẏa jaẏa jaẏa hē.
+</textarea> 
+
+<textarea name="message" rows="10" cols="30">
+          !! NOTICE!!
+         
+
+</textarea>
+
+</div>
+ 
+
+@endsection
